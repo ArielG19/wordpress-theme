020e0398851312c9c4f5ed089e6256168afc30f5
Ruben <gotze19ariel@gmail.com>
2021-11-17 18:30:37 +0100

    creando page template sin sidebar

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -1,43 +1,37 @@
 <?php
-/*
-* Template Name: Plantilla (No Sidebars)
-*/
-    get_header();
-?>
+    /*
+    * Template Name: Contenido Centrado (No Sidebar)
+    */
+     get_header();  ?>
 
 
+    <main class="contenedor no-sidebar">
+        <div class="contenido-principal">
+            <?php
+                while(have_posts()): the_post();
+            ?>
 
-    <main class="content">
-    
-        <?php
-            while(have_posts()): the_post();
-        ?>
+            <h1> <?php the_title(); ?> </h1>
 
-        <h1>
-            <?php the_title(); ?>
-        </h1>
+            <?php
+                //Si hay imagen destacada
+                if(has_post_thumbnail()):
+                    //cargamos nuestra imagen con tamaño personalizado en nuestro function
+                    //insertamos tambien nuestra propia clase a la imagen
+                    the_post_thumbnail('blog',array('class' => 'imagen-destacada'));
+                endif;
+            ?>
 
-        <?php
-            //Si hay imagen destacada
-            if(has_post_thumbnail()):
-                //cargamos nuestra imagen con tamaño personalizado en nuestro function
-                //insertamos tambien nuestra propia clase a la imagen
-                the_post_thumbnail('blog',array('class' => 'imagen-destacada'));
-            endif;
-        ?>
+            <p><?php the_content(); ?></p>
+            <p>Escrito por: <?php the_author(); ?></p>
+            <p>Fecha: <?php the_date(); ?></p>
+            
 
-        <p>
-            <?php the_content(); ?>
-        </p>
-
-        Escrito por: <?php the_author(); ?>
-
-        Fecha: <?php the_date(); ?>
-
-        <?php
-            endwhile;
-        ?>
-
+            <?php
+                endwhile;
+            ?>
+        </div>
+        
         <hr>
     </main>
 <?php get_footer();  ?>
