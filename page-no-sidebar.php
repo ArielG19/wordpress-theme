9cd6b0ac9f0d3f5790c6b528c7e500c5944ebe8b
Ruben <gotze19ariel@gmail.com>
2021-11-17 22:36:30 +0100

    mejorando parte responsive

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -7,29 +7,8 @@
 
     <main class="contenedor no-sidebar">
         <div class="contenido-principal">
-            <?php
-                while(have_posts()): the_post();
-            ?>
-
-            <h1> <?php the_title(); ?> </h1>
-
-            <?php
-                //Si hay imagen destacada
-                if(has_post_thumbnail()):
-                    //cargamos nuestra imagen con tamaño personalizado en nuestro function
-                    //insertamos tambien nuestra propia clase a la imagen
-                    the_post_thumbnail('blog',array('class' => 'imagen-destacada'));
-                endif;
-            ?>
-
-            <p><?php the_content(); ?></p>
-            <p>Escrito por: <?php the_author(); ?></p>
-            <p>Fecha: <?php the_date(); ?></p>
-            
-
-            <?php
-                endwhile;
-            ?>
+            <!--Cargando loop/contenido de pagina-->
+          <?php get_template_part('template-parts/loop-page'); ?>
         </div>
         
         <hr>
