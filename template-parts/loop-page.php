0adfb5dd03caf80a27770a1650247b60a582d946
Ruben <gotze19ariel@gmail.com>
2021-11-26 12:31:33 +0100

    agregando template clases-detalle, modificando loop de post

--- template-parts/loop-page.php
+++ template-parts/loop-page.php
@@ -12,6 +12,20 @@
             endif;
         ?>
 
+        <?php
+            //Revisar si el custom post es Clase 
+            if(get_post_type() == 'gymfitness_clases'){
+                   //Utilizando los campos personalizados advance custom field
+                    $h_inicio = get_field('hora_inicio');
+                    $h_fin = get_field('hora_fin');
+        ?>
+                    <p class="informacion-clase"> <?php the_field('dias_clase');?> / <?php echo $h_inicio . " - " . $h_fin;  ?> </p>
+        <?php  
+             }
+
+        ?>
+
+
         <div class="text-content">
             <?php the_content(); ?> 
             
